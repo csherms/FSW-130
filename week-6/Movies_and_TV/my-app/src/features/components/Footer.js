9bc9b57c310cd41752c7ad683f1fb07035acbf9d
csherms <casey.sherman@bryanuniversity.edu>
2021-05-28 06:58:14 -0600

    first commit

---
+++ week-6/Movies_and_TV/my-app/src/features/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.css";
+
+export function Footer() {
+  return (
+    <footer className='footer-wrapper'>
+      <p>Casey Sherman</p>
+      <p>FSW-130</p>
+      <p>Week 6 Movies and Tv Show list</p>
+    </footer>
+  );
+}
